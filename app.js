735223371b71edb326fc9336315a6ca5dcce4c35
Sam Khosravi <khsam@outlook.com>
2024-04-05 19:34:28 +0200

    Commit avec les fichiers qui fonctionnent bien

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-Quagga.init({
-    inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector('#interactive'),
-        constraints: {
-            facingMode: "environment"
-        },
-    },
-    decoder: {
-        readers: ["code_128_reader", "ean_reader", "ean_8_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "upc_reader", "upc_e_reader"]
-    },
-}, function (err) {
-    if (err) {
-        console.log(err);
-        return
+function sendBarcode() {
+    const barcode = document.getElementById('barcodeInput').value;
+    if (barcode) {
+        fetch('http://127.0.0.1:5000/scan', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                barcode: barcode,
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                alert('Barcode sent successfully!');
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                alert('Failed to send barcode.');
+            });
+    } else {
+        alert('Please enter a barcode.');
     }
-    console.log("Initialization finished. Ready to start");
-    Quagga.start();
-});
-
-Quagga.onDetected(function (data) {
-    fetch('http://127.0.0.1:5000/scan', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            barcode: data.codeResult.code,
-        }),
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-});
+}
