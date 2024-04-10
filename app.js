52bbfde886d17e097466455c17af057a0e56d4cc
SamKhosravi <127540394+SamKhosravi@users.noreply.github.com>
2024-04-10 10:05:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-function onScanSuccess(decodedText, decodedResult) {
-    // Utiliser `decodedText` pour obtenir le texte du code QR/Code-barres scanné.
-    console.log(`Code QR/Code-barres scanné = ${decodedText}`, decodedResult);
-    alert(`Code QR/Code-barres scanné: ${decodedText}`);
-    // Arrêter le scanner une fois qu'un code est scanné. Retirer si vous souhaitez continuer à scanner.
-    html5QrcodeScanner.clear();
-}
+document.addEventListener('DOMContentLoaded', function () {
+    Quagga.init({
+        inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector('#reader'), // Assurez-vous que cet élément existe dans votre HTML
+            constraints: {
+                facingMode: "environment"
+            },
+        },
+        decoder: {
+            readers: [
+                "code_128_reader",
+                "ean_reader",
+                "ean_8_reader",
+                "code_39_reader",
+                "upc_reader",
+                "upc_e_reader",
+                "codabar_reader",
+                "i2of5_reader",
+                "2of5_reader",
+                "code_93_reader",
+                // Ajoutez d'autres lecteurs de codes-barres selon vos besoins
+            ]
+        },
+        locate: true // Utilisez l'emplacement pour afficher visuellement où le code-barres est détecté
+    }, function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        Quagga.start();
+    });
 
-function onScanFailure(error) {
-    // Ecrire le traitement en cas d'erreur de scan.
-    console.warn(`Échec de scan QR/Code-barres: ${error}`);
-}
-
-// Configurer les options du scanner. Vérifier la documentation pour plus d'options.
-let config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
-// Créer une nouvelle instance de `Html5QrcodeScanner`
-let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    Quagga.onDetected(function (data) {
+        console.log(data.codeResult.code);
+        // Traitez le code détecté ici.
+    });
+});
