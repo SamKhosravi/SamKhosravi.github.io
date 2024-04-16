9bfa610c5357e03ba8a75533f4396c6dfd107a74
SamKhosravi <127540394+SamKhosravi@users.noreply.github.com>
2024-04-16 15:34:20 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAWrPPe2PF-i-zCgarDafqQ5ywS5OQQ5k8",
-    authDomain: "nutri-scan-food.firebaseapp.com",
-    projectId: "nutri-scan-food",
-    storageBucket: "nutri-scan-food.appspot.com",
-    messagingSenderId: "451956998080",
-    appId: "1:451956998080:web:98a4fb3bf0b2148a5b1225",
-    measurementId: "G-PBH8J37N2K"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const facebookProvider = new FacebookAuthProvider();
+<script type="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+    import { getAuth, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+    
+    const firebaseConfig = {
+        apiKey: "AIzaSyAWrPPe2PF-i-zCgarDafqQ5ywS5OQQ5k8",
+        authDomain: "nutri-scan-food.firebaseapp.com",
+        projectId: "nutri-scan-food",
+        storageBucket: "nutri-scan-food.appspot.com",
+        messagingSenderId: "451956998080",
+        appId: "1:451956998080:web:98a4fb3bf0b2148a5b1225",
+        measurementId: "G-PBH8J37N2K"
+    };
+    
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+</script>
 
 document.getElementById('sign-in-google').addEventListener('click', () => {
     signInWithPopup(auth, googleProvider)
@@ -28,13 +29,3 @@ document.getElementById('sign-in-google').addEventListener('click', () => {
         });
 });
 
-document.getElementById('sign-in-facebook').addEventListener('click', () => {
-    signInWithPopup(auth, facebookProvider)
-        .then((result) => {
-            console.log(result.user);
-            // Redirection to index.html after successful login
-            window.location.assign('index.html');
-        }).catch((error) => {
-            console.error('Failed to sign in with Facebook:', error);
-        });
-});
