04f0634b6e61d9d54d150ddaed6e0b098d7887ac
SamKhosravi <127540394+SamKhosravi@users.noreply.github.com>
2024-04-16 11:54:41 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,34 +1,3 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
-const facebookProvider = new FacebookAuthProvider();
-
-document.getElementById('sign-in-google').addEventListener('click', () => {
-    signInWithPopup(auth, googleProvider)
-        .then((result) => {
-            // Gestion réussie
-            console.log('Google sign in success', result);
-        })
-        .catch((error) => {
-            // Gestion des erreurs
-            console.error('Google sign in error', error);
-        });
-});
-
-document.getElementById('sign-in-facebook').addEventListener('click', () => {
-    signInWithPopup(auth, facebookProvider)
-        .then((result) => {
-            // Gestion réussie
-            console.log('Facebook sign in success', result);
-        })
-        .catch((error) => {
-            // Gestion des erreurs
-            console.error('Facebook sign in error', error);
-        });
-});
-
-
 function onScanSuccess(decodedText, decodedResult) {
     // Insérer le code scanné dans le champ de saisie manuelle.
     document.getElementById('manual-input').value = decodedText;
